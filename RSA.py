f1acd6b0f875dcbba2996a3c35161387dbf92ac9
Sondre Pettersen <sondre.pettersen@proton.me>
2025-09-26 09:19:00 +0200

    Add 256-bit support

--- RSA.py
+++ RSA.py
@@ -5,13 +5,15 @@
 #   Where C is the encrypted message, M is the message,
 #   n is p*q (product of prime numbers so that M < n) and e and d are public/private exponents
 import math
+from sympy import randprime
+import secrets
 
-M = 13376969
+M = secrets.randbits(256)
 C = 0
 
 def rsa_key_generation():
-    p = 4079
-    q = 5449
+    p = randprime(2**127, 2**128)
+    q = randprime(2**127, 2**128)
     n = p*q  
     phi_n = (p-1)*(q-1)
     e = 65537
@@ -23,6 +25,8 @@ def rsa_key_generation():
     print("Private key:", d, n)
     return e, d, n
 
+###############################################
+# This part will be implemented in VHDL
 def blakley_mul(a, b, n):
     r = 0
     for i in range(b.bit_length()-1, -1, -1):
@@ -31,22 +35,27 @@ def blakley_mul(a, b, n):
             r = (r + a) % n
     return r
 
-def modexp_blakley(base, exponent, n):
+def modexp_RL_method(base, exponent, n):
     result = 1
-    p = base % n
+    base = base % n
     e = exponent
     while e > 0:
         if e & 1:
-            result = blakley_mul(result, p, n)
-        p = blakley_mul(p, p, n)
+            result = blakley_mul(result, base, n)
+        base = blakley_mul(base, base, n)
         e = e >> 1
     return result
-
-
+#
+################################################
 e, d, n = rsa_key_generation()
 
-C = modexp_blakley(M, e, n)
+C = modexp_RL_method(M, e, n)
 print("Original message:", M)
 print("Encrypted message:", C)
-M = modexp_blakley(C, d, n)
+M_decrypted = modexp_RL_method(C, d, n)
 print("Decrypted message:", M)
+
+if (M == M_decrypted):
+    print("Success!")
+else:
+    print("Not success :(")
