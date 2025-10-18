0c08510c72608bd10a3f7fd617ec8a7add7fc5f0
Nikolai Markussen <nikolai.markussen01@gmail.com>
2025-10-18 16:06:33 +0200

    lagt inn top_level, rsa_fsm, og minor endringer i microarch

---
+++ rsa_fsm.vhd
@@ -0,0 +1,163 @@
+-- ============================================================
+-- rsa_fsm.vhd
+-- FSM idè
+-- ============================================================
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity fsm is
+    port (
+        clk, reset        : in  std_logic;
+
+        -- AXIS handshakes med moduler utenfra rsa_core
+        msgin_valid       : in  std_logic;                      -- rsa_msgin sier at den har en mld klar til å hentes av rsa_core
+        msgin_ready       : out std_logic;                      -- rsa_core sier den er klar til å motta mld fra rsa_msgin
+        msgout_valid      : out std_logic;                      -- rsa_core sier at den har en mld klar til å hentes av rsa_msgout
+        msgout_ready      : in  std_logic;                      -- rsa_msgout sier at den er klar til å lese mld fra rsa_core
+        msgout_last       : out std_logic;                      -- flagg som må være høyt når meldinger skal sendes ut
+
+        -- Datapath feedback fra submoduler
+        exp_lsb           : in  std_logic;                      -- signal som sier hvilken verdi neste lsb i eksponenten er
+        exp_zero          : in  std_logic;                      -- flagg som går høyt når hele eksponenten er null, altså meldingen er ferdig prosessert og kan sendes ut til rsa_msgout
+        blakley_done      : in  std_logic;                      -- flagg som settes høyt av blakley.vhd når den har utført operasjonen
+
+        -- Control outputs til rsa_top_level
+        mux_base_sel      : out std_logic_vector(1 downto 0);   -- tobits vektor som sier om blakley operasjonen som skal utføres er base eller result
+        demux_result_sel    : out std_logic;                      -- eeeeeeeeeeeeeeeehm
+        mux_exp_sel       : out std_logic;                      -- kontrollsignal til bitshift MUX'en
+        shift_enable      : out std_logic;                      -- signal som sier om bitshift skal utføres eller ikke
+        blakley_start     : out std_logic                       -- signal som sier om blakley skal utøres eller ikke
+    );
+end fsm;
+
+architecture behavioral of fsm is
+    type state_type is (
+        IDLE, LOAD, INIT, CHECK_BIT,
+        MULT_RESULT, SQUARE_BASE, SHIFT_EXP,
+        DONE, WAIT_READY
+    );
+    signal state, next_state : state_type;
+begin
+    ------------------------------------------------------------
+    -- Prosess som kjører state machinen
+    ------------------------------------------------------------
+    process(clk, reset)
+    begin
+        if reset = '1' then
+            state <= IDLE;
+        elsif rising_edge(clk) then
+            state <= next_state;
+        end if;
+    end process;
+
+    ------------------------------------------------------------
+    -- Next-state og output logikk
+    ------------------------------------------------------------
+    process(state, msgin_valid, msgout_ready, exp_lsb, exp_zero, blakley_done)
+    begin
+        -- Default outputs
+        msgin_ready      <= '0';
+        msgout_valid     <= '0';
+        msgout_last      <= '0';
+        mux_base_sel     <= "00";
+        demux_result_sel   <= '0';
+        mux_exp_sel      <= '0';
+        shift_enable     <= '0';
+        blakley_start    <= '0';
+        next_state       <= state;
+
+        case state is
+
+            ------------------------------------------------
+            -- IDLE: Venter på msgin_valid
+            ------------------------------------------------
+            when IDLE =>
+                msgin_ready <= '1';                             -- sier ifra at vår enhet er klar til å motta signal
+                if msgin_valid = '1' then                       -- triggres når msgin_valid blir 1, dette bestemmes av rsa_msgin
+                    next_state <= LOAD;
+                end if;
+
+            ------------------------------------------------
+            -- LOAD: Lar det gå en klokkesyklus før INIT begynner, for å unngå overlappende register-oppdateringer. Ingen annen funksjonalitet. Kan droppes?
+            ------------------------------------------------
+            when LOAD =>
+                mux_base_sel <= "00";
+                next_state   <= INIT;
+
+            ------------------------------------------------
+            -- INIT: Initialiserer result slik at result * base mod n != 0
+            ------------------------------------------------
+            when INIT =>
+                demux_result_sel <= '1';
+                next_state <= CHECK_BIT;
+
+            ------------------------------------------------
+            -- CHECK_BIT: Sjekker eksponent bitten
+            ------------------------------------------------
+            when CHECK_BIT =>
+                if exp_lsb = '1' then                           -- sjekker om eksponent bitshiften er 1
+                    next_state <= MULT_RESULT;                  -- om ja, gå inn i result * base mod n operasjonen
+                else
+                    next_state <= SQUARE_BASE;                  -- om nei, gå inn i vanlig base * base mod n operasjonen
+                end if;
+
+            ------------------------------------------------
+            -- MULT_RESULT: result = result * base mod n
+            ------------------------------------------------
+            when MULT_RESULT =>
+                blakley_start <= '1';                           -- sier til blakley.vhd at den skal starte result * base mod n operasjonen
+                mux_base_sel  <= "01";                          -- dette signalet indikerer kun hvilken state den er i, kan ses på for feilsøking
+                if blakley_done = '1' then                      -- venter på at blakley.vhd skal si den er ferdig
+                    next_state <= SQUARE_BASE;
+                end if;
+
+            ------------------------------------------------
+            -- SQUARE_BASE: base = base * base mod n
+            ------------------------------------------------
+            when SQUARE_BASE =>
+                blakley_start <= '1';                           -- sier til blakley at den skal starte base * base mod n operasjonen
+                mux_base_sel  <= "10";                          -- 10 indikerer vanlig base mod
+                if blakley_done = '1' then                      -- venter på at blakley.vhd skal si den er ferdig
+                    next_state <= SHIFT_EXP;
+                end if;
+
+            ------------------------------------------------
+            -- SHIFT_EXP: bitshift state
+            ------------------------------------------------
+            when SHIFT_EXP =>
+                shift_enable <= '1';                            -- Sier til bitshift.vhd at den skal utføre
+                mux_exp_sel  <= '1';                            -- bitshift MUX'en settes til 1, slik at den repeater eksponenten tilbake etter den har blitt bitshifta
+                if exp_zero = '1' then                          -- exp_zero sier at hele eksponenten har blitt bitshifta, som betyr at meldingen er ferdig prosessert. Går til DONE
+                    next_state <= DONE;
+                else
+                    next_state <= CHECK_BIT;                    -- exp_zero sier at ikke hele eksponenten har blitt bitshifta, som betyr at meldingen ikke er ferdig prosessert enda. Går tilbake til CHECK_BIT.
+                end if;
+
+            ------------------------------------------------
+            -- DONE: Output klar til å hentes
+            ------------------------------------------------
+            when DONE =>
+                msgout_valid <= '1';                            -- Sier til rsa_msgout at msgout_data er klar til å hentes
+                msgout_last  <= '1';                            -- Sier til rsa_msgout at dette er siste gyldige dataordet i en gitt pakke. Skal dras høy på samme klokkesyklus som 256-bit meldingen overføres.
+                if msgout_ready = '1' then          
+                    next_state <= IDLE;                         -- Hvis rsa_msgout er klar, kan FSM gå tilbake til IDLE for å kunne begynne med neste mld.
+                else
+                    next_state <= WAIT_READY;                   -- Hvis ikke så venter FSM'en på rsa_msgout
+                end if;
+
+            ------------------------------------------------
+            -- WAIT_READY: Venter på at rsa_msgout skal bli klar
+            ------------------------------------------------
+            when WAIT_READY =>
+                msgout_valid <= '1';                            -- msgout_data er fremdeles klar til å hentes
+                msgout_last  <= '1';                            -- msgout_last er fremdeles høy, denne staten holder FSM'en til rsa_msgout har hentet meldingen
+                if msgout_ready = '1' then
+                    next_state <= IDLE;                         -- melding hentet, tilbake til start
+                end if;
+
+            when others =>
+                next_state <= IDLE;
+        end case;
+    end process;
+end behavioral;
---
+++ rsa_top_level.vhd
@@ -0,0 +1,184 @@
+-- =============================================================================
+-- rsa_top_level.vhd
+-- top level utkast, blakley og bitshift moduler sine signaler kan endres på i portmappen nede
+-- =============================================================================
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+-- Bare signalene inn og ut av RSA core, blodkopi fra word dokumentet sin figur 5, pluss clk og reset
+entity rsa_top_level is
+    port (
+        clk            : in  std_logic;
+        reset          : in  std_logic;
+
+        -- AXIS input (til/fra rsa_msgin)
+        msgin_valid    : in  std_logic;
+        msgin_ready    : out std_logic;
+        msgin_last     : in  std_logic;
+        msgin_data     : in  std_logic_vector(255 downto 0);
+
+        -- AXIS output (til/fra rsa_msgout)
+        msgout_valid   : out std_logic;
+        msgout_ready   : in  std_logic;
+        msgout_last    : out std_logic;
+        msgout_data    : out std_logic_vector(255 downto 0);
+
+        -- Signalene til/fra rsa_regio
+        key_e          : in  std_logic_vector(255 downto 0);
+        key_n          : in  std_logic_vector(255 downto 0);
+        rsa_status     : out std_logic_vector(31 downto 0)
+    );
+end rsa_top_level;
+
+architecture rtl of rsa_top_level is
+
+    -------------------------------
+    -- Interne kontrollsignaler
+    -------------------------------
+    signal blakley_start   : std_logic;
+    signal blakley_done    : std_logic;
+    signal shift_enable    : std_logic;
+
+    signal exp_lsb         : std_logic;
+    signal exp_zero        : std_logic;
+
+    -- Multiplexer kontrollsignaler
+    signal mux_base_sel    : std_logic_vector(1 downto 0);
+    signal demux_result_sel  : std_logic;
+    signal mux_exp_sel     : std_logic;
+
+    -- Interne data-registere
+    signal base_reg        : std_logic_vector(255 downto 0);
+    signal exp_reg         : std_logic_vector(255 downto 0);
+    signal mod_n_reg       : std_logic_vector(255 downto 0);
+    signal result_reg      : std_logic_vector(255 downto 0);
+
+    -- Blakley resultat
+    signal blakley_result  : std_logic_vector(255 downto 0);
+
+begin
+    ---------------------------------------------------------------
+    -- FSM instansiering og portmap, signalene er forklart i rsa_fsm.vhd
+    ---------------------------------------------------------------
+    u_fsm : entity work.rsa_fsm                    -- tror ikke work. funker i VSCode, kanskje hvis vi lager ordentlig mappestruktur
+        port map (
+            clk           => clk,
+            reset         => reset,
+
+            -- Handshakes
+            msgin_valid   => msgin_valid,
+            msgin_ready   => msgin_ready,
+            msgout_valid  => msgout_valid,
+            msgout_ready  => msgout_ready,
+            msgout_last   => msgout_last,
+
+            -- Datapath feedback
+            exp_lsb       => exp_lsb,
+            exp_zero      => exp_zero,
+            blakley_done  => blakley_done,
+
+            -- Control outputs
+            mux_base_sel  => mux_base_sel,
+            demux_result_sel=> demux_result_sel,
+            mux_exp_sel   => mux_exp_sel,
+            shift_enable  => shift_enable,
+            blakley_start => blakley_start
+        );
+
+    ---------------------------------------------------------------
+    -- Blakley modul instansiering og portmap, hvis dere vil at signalene skal hete noe annet i deres moduler, endre venstre side
+    ---------------------------------------------------------------
+    u_blakley : entity work.blakley
+        port map (
+            clk      => clk,
+            reset    => reset,
+            start    => blakley_start,
+            a        => base_reg,
+            b        => result_reg,
+            n        => mod_n_reg,
+            done     => blakley_done,
+            result   => blakley_result
+        );
+
+    ---------------------------------------------------------------
+    -- Bitshift modul instansiering og portmap, hvis dere vil at signalene skal hete noe annet i deres moduler, endre venstre side
+    ---------------------------------------------------------------
+    u_bitshift : entity work.bitshift
+        port map (
+            clk       => clk,
+            reset     => reset,
+            enable    => shift_enable,
+            data_in   => exp_reg,
+            data_out  => exp_reg,
+            lsb_out   => exp_lsb,
+            zero_out  => exp_zero
+        );
+
+------------------------------------------------------------
+-- Datapath register prosess
+-- Kontrollerer base, eksponent, modulo, result, og output data
+------------------------------------------------------------
+process(clk, reset)
+begin
+    if reset = '1' then                         -- asynkron reset, burde være der ved oppstart slik at man forsikrer seg at FSM går inn i IDLE ved power-up
+        base_reg     <= (others => '0');
+        exp_reg      <= (others => '0');
+        mod_n_reg    <= (others => '0');
+        result_reg   <= (others => '0');
+        msgout_data  <= (others => '0');
+        rsa_status   <= (others => '0');
+
+    elsif rising_edge(clk) then
+        ------------------------------------------------
+        -- 1. Last inn ny mld og nøkler (LOAD)
+        ------------------------------------------------
+        if (msgin_valid = '1' and msgin_ready = '1') then
+            -- Laster base (melding), eksponent, og modulo
+            base_reg   <= msgin_data;
+            exp_reg    <= key_e;
+            mod_n_reg  <= key_n;
+        end if;
+
+        ------------------------------------------------
+        -- 2. Initialiser result = 1 (INIT)
+        ------------------------------------------------
+        if (demux_result_sel = '1') then
+            result_reg <= (others => '0');
+            result_reg(0) <= '1';               -- Setter result_reg til 1 slik at result * base ikke blir 0
+        end if;
+
+        ------------------------------------------------
+        -- 3. Oppdaterer enten base eller result registeret etter blakley operasjon er utført (MULT_RESULT og SQUARE_BASE)
+        ------------------------------------------------
+        if (blakley_done = '1') then
+            -- FSM sier ifra hva slags operasjon som ble utført:
+            -- mux_base_sel velger hvor resultatet skal lagres
+            if mux_base_sel = "10" then
+                -- SQUARE_BASE state --> oppdater base_reg
+                base_reg <= blakley_result;
+            elsif mux_base_sel = "01" then
+                -- MULT_RESULT state --> oppdater result_reg
+                result_reg <= blakley_result;
+            end if;
+        end if;
+
+        ------------------------------------------------
+        -- 4. Shifte eksponenten (SHIFT_EXP) (utført av bitshift.vhd)
+        ------------------------------------------------
+        if (shift_enable = '1') then
+            exp_reg <= std_logic_vector(shift_right(unsigned(exp_reg), 1));
+        end if;
+
+        ------------------------------------------------
+        -- 5. Output final result (DONE)
+        ------------------------------------------------
+        if (msgout_valid = '1' and msgout_ready = '1') then
+            msgout_data <= result_reg;
+            rsa_status  <= (others => '0');  -- Denne brukes ikke enda, vet ikke helt hva vi skal med den? Den assignes for å unngå errors under synthesis
+        end if;
+    end if;
+end process;
+
+
+end rtl;
