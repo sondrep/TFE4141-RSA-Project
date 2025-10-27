2703ee778a356137a48e6b602de35a38f37f493c
Sondre Pettersen <sondre.pettersen@proton.me>
2025-10-27 10:24:46 +0100

    Add support for 256-bit width

--- VHDL/src/blakley_mul.vhd
+++ VHDL/src/blakley_mul.vhd
@@ -1,4 +1,3 @@
--- blakley_mul.vhd
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
@@ -23,11 +22,16 @@ end entity;
 architecture rtl of blakley_mul is
     signal a_reg, b_reg, n_reg : unsigned(WIDTH-1 downto 0);
     signal r_reg               : unsigned(WIDTH-1 downto 0);
-    signal bit_index           : integer range 0 to WIDTH;
+    signal bit_index           : integer range 0 to WIDTH-1;
     signal running             : std_logic := '0';
 begin
     process(clk, rst)
-        variable tmp : unsigned(WIDTH downto 0);
+        -- tmp is WIDTH+1 bits to hold shifted r and additions without overflow
+        variable tmp    : unsigned(WIDTH downto 0);
+        -- r_next holds the next value of r (WIDTH bits)
+        variable r_next : unsigned(WIDTH-1 downto 0);
+        -- n_ext is modulus extended to WIDTH+1 for easy comparisons
+        variable n_ext  : unsigned(WIDTH downto 0);
     begin
         if rst = '1' then
             a_reg     <= (others => '0');
@@ -41,8 +45,8 @@ begin
         elsif rising_edge(clk) then
             done <= '0';
 
-            -- Start signal initializes multiplication
             if start = '1' and running = '0' then
+                -- Load inputs. Caller should ensure A < N.
                 a_reg     <= unsigned(A);
                 b_reg     <= unsigned(B);
                 n_reg     <= unsigned(N);
@@ -52,23 +56,36 @@ begin
                 busy      <= '1';
 
             elsif running = '1' then
-                -- 1. Shift left: r = (r << 1) % n
-                tmp := shift_left(r_reg, 1);
-                if tmp >= resize(n_reg, WIDTH+1) then
-                    tmp := tmp - resize(n_reg, WIDTH+1);
-                end if;
-                r_reg <= tmp(WIDTH-1 downto 0);
+                -- Prepare extended modulus for compares
+                n_ext := resize(n_reg, WIDTH+1);
 
-                -- 2. If current bit of b is 1: r = (r + a) % n
+                -- Start from current r
+                r_next := r_reg;
+
+                -- 1) Shift-left r: tmp = (r << 1)
+                tmp := shift_left(resize(r_next, WIDTH+1), 1);
+
+                -- Reduce once if needed: tmp = tmp - n if tmp >= n
+                if tmp >= n_ext then
+                    tmp := tmp - n_ext;
+                end if;
+
+                -- Update r_next to the shifted/reduced value
+                r_next := tmp(WIDTH-1 downto 0);
+
+                -- 2) If current bit of b is 1: r = (r + a) % n
                 if b_reg(bit_index) = '1' then
-                    tmp := resize(r_reg, WIDTH+1) + resize(a_reg, WIDTH+1);
-                    if tmp >= resize(n_reg, WIDTH+1) then
-                        tmp := tmp - resize(n_reg, WIDTH+1);
+                    tmp := resize(r_next, WIDTH+1) + resize(a_reg, WIDTH+1);
+                    if tmp >= n_ext then
+                        tmp := tmp - n_ext;
                     end if;
-                    r_reg <= tmp(WIDTH-1 downto 0);
+                    r_next := tmp(WIDTH-1 downto 0);
                 end if;
 
-                -- 3. Decrement bit index
+                -- Commit once
+                r_reg <= r_next;
+
+                -- 3) Decrement bit index / finish
                 if bit_index = 0 then
                     running <= '0';
                     busy    <= '0';
