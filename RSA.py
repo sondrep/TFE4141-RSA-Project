cb7b9c0f726d40a2cd5269a09fc98bf9a180ff1d
stusle <st.os.slet@gmail.com>
2025-11-14 13:04:07 +0100

    Add comments

--- RSA.py
+++ RSA.py
@@ -4,13 +4,15 @@
 #   Decryption: M = C^d mod(n)
 #   Where C is the encrypted message, M is the message,
 #   n is p*q (product of prime numbers so that M < n) and e and d are public/private exponents
+
 import math
 from sympy import randprime
 import secrets
 
-M = secrets.randbits(256)
-C = 0
+M = secrets.randbits(256) # Original message
+C = 0                     # Encrypted message
 
+# Function for generating keys
 def rsa_key_generation():
     p = randprime(2**127, 2**128)
     q = randprime(2**127, 2**128)
@@ -27,6 +29,8 @@ def rsa_key_generation():
 
 ###############################################
 # This part will be implemented in VHDL
+
+# Function for blakley multiplication
 def blakley_mul(a, b, n):
     r = 0   
     for i in range(b.bit_length()-1, -1, -1):
@@ -35,26 +39,38 @@ def blakley_mul(a, b, n):
             r = (r + a) % n
     return r
 
+# Function for doing modular exponentiation with blakley multiplication
 def modexp_RL_method(base, exponent, n):
+    
+    # Init result, base, and exponent
     result = 1
     base = base % n
     e = exponent
+
+    # Run blakley mul while exponent is greater than zero
     while e > 0:
         if e & 1:
             result = blakley_mul(result, base, n)
         base = blakley_mul(base, base, n)
         e = e >> 1
     return result
+
 #
 ################################################
+
+# Generate keys
 e, d, n = rsa_key_generation()
 
+# Encrypt message
 C = modexp_RL_method(M, e, n)
 print("Original message:", M)
 print("Encrypted message:", C)
+
+# Decrypt message
 M_decrypted = modexp_RL_method(C, d, n)
 print("Decrypted message:", M_decrypted)
 
+# Check if decrypted message equals original message
 if ((M % n) == M_decrypted):
     print("Success!")
 else:
